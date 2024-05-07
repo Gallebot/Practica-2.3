bcfc252a0e18ee19fb17b71bc99baebf105f4826
“samuel” <samtorez84@gmail.com>
2024-05-07 08:21:38 -0600

    Commit Samuel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
+//import DB from "./config/db.js";
 
 dotenv.config();
 
@@ -7,6 +8,34 @@ const app = express();
 const port = process.env.PORT || 3000;
 console.log(port);
 
+app.get("/", (req,res) => {
+    res.send("Hola bienvenidos")
+})
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+
+/*
+import express from "express";
+import dotenv from 'dotenv';
+import DB from "./config/db.js";
+import ProductRouter from "./routes/ProductRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+console.log(port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", ProductRouter);  //Archivos de rutas para cada una de las entidades. Modelado por cada uno de los registros. rimer punto de mi ruta, se va concatenando
+                                    
+DB.connectDB(process.env.DB_URI);
+
+app.listen(port, () => {
+    console.log(Server running at http://localhost:${port});
+});
+*/
