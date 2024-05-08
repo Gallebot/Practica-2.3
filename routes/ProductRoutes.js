a1d678fc3326870f62602a069e2d09c9984b7296
“samuel” <samtorez84@gmail.com>
2024-05-08 09:02:55 -0600

    Commit Samuel

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -19,16 +19,17 @@ router.post("/products", async (request, response) => {
     const product = new ProductModel(request.body); //Se accede al cuerpo en request.body. El body viene como un objeto de tipo json, los datos que se deben emviar vienen en el product.js
 
     try {
-        await product.save();
-        response.send(product);
+        await product.save();  //Guarda los valores
+        response.send(product); //Mando una respuesta con un id 
     } catch (error) {
         response.status(500).send(error);
     }
 });
 
-router.get("/products/:id", async (request, response) => {
+//Filtarr con un id
+router.get("/products/:id", async (request, response) => {  //:varibale es un valor que se va a recibir, que cambia y que se le asigna el nombre de id
     try {
-        const product = await ProductModel.findOne({ _id: request.params.id });
+        const product = await ProductModel.findOne({ _id: request.params.id }); //Accede a la petición a través de .params. A través del modelo y el método "Encuentra uno", se busca el primer paramatro _id que oincida con :id
         response.send(product);
     } catch (error) {
         response.status(500).send({ error });
